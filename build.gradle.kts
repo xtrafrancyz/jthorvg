b1564751be58ebf8609ee91fec1799655a851d07
Dmytro Manchynskyi <xtrafrancyz@gmail.com>
2026-07-16 20:45:32 +0300

    fix the name of central repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -367,7 +367,7 @@ signing {
 
 nexusPublishing {
     repositories {
-        create("myNexus") {
+        create("SonatypeCentral") {
             nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
             snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
             username.set(System.getenv("SONATYPE_USERNAME") ?: "")
